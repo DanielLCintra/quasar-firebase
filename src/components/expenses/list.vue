9eb6b364bf79f27534f3b67e34013ce46a5ffb5e
Daniel Loureiro Cintra <cintra.70@gmail.com>
2017-04-24 23:50:49 -0300

    arquivos do projeto

---
+++ src/components/expenses/list.vue
@@ -0,0 +1,77 @@
+<script>
+
+	import { Dialog } from 'quasar'
+	import { setDone} from '../../persistence'
+
+	export default{
+
+		computed:{
+			list(){
+				return this.$store.state.Expenses.list
+			}
+		},
+		methods:{
+			askRemove(expense){
+
+				const VmThis = this
+
+				Dialog.create({
+				  title: 'Tem certeza?',
+				  buttons: [
+				    {
+				      label: 'Cancelar'
+				    },
+				    {
+				      label: 'Confirmar',
+				      handler () {
+				        VmThis.remove(expense)
+				      }
+				    }
+				  ]
+				})
+			},
+			remove(expense){
+
+				this.$store.commit('REMOVE_EXPENSE', expense)
+			
+			},
+			toggle(expense){
+				
+				expense.done = !expense.done
+
+				setDone(expense)
+				
+			}
+		}
+	}
+</script>
+
+<template>	
+	<div>
+		<div class="expense" v-for="expense in list" @click="toggle(expense)">
+			<p :class="{ done: expense.done }">{{ expense.date }} - R${{ expense.amount }}</p>
+			<p :class="{ done: expense.done }">{{ expense.description }}</p>
+			<a class="removeLink" href="#" @click.prevent="askRemove(expense)"> remove</a>
+		</div>	
+	</div>
+</template>
+
+<style>
+	.done{
+		text-decoration: line-through;
+		color: #ccc;
+	}	
+	.expense{
+		border-bottom: #999 1px solid;
+		padding-top: 10px;
+		position: relative;
+		cursor: pointer;
+	}
+	.removeLink{
+		color: #c00000;
+		position: absolute;
+		bottom: 10px;
+		right: 0;
+		font-size: 0.8em;
+	}
+</style>
